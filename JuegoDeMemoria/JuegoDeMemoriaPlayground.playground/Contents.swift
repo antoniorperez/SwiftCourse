0ab6b12df67d0058d58b07afe02aab48b5d51d58
Antonio Rodríguez <antoniorodriguez@imac>
2016-01-07 22:27:54 +0100

    Eliminados ficheros de proyecto innecesarios y añadido el playground

---
+++ JuegoDeMemoria/JuegoDeMemoriaPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground para ejercicio de Juego de Memoria de la semana 2 del curso sobre programación para swift
+
+import UIKit
+
+
+//Creamos el rango de 0 a 100
+var rango = 1...100
+print(rango)
+
+
+//Iteramos por el rango
+for num in rango{
+    
+    //Si es multiplo de 5
+    if num % 5 == 0{
+        print("\(num) Bingo!!!")
+    //Si es par
+    }else if num % 2 == 0 {
+        print("\(num) par!!!")
+    //Si es impar
+    }else if num % 2 != 0{
+        print("\(num) impar!!!")
+    //Si está entre 30 y 40 (ambos inclusive)
+    }else if num >= 30 && num <= 40{
+        print("\(num) Viva Swift!!!")
+    }
+    
+}
